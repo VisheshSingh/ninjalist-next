261a3ea8c427e43d0fef41ed6efdf94352e5c27f
Vishesh Singh <VisheshSingh16@gmail.com>
2021-02-02 21:21:09 -0600

    Redirect ↩

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const NotFound = () => {
+  const router = useRouter();
+  useEffect(() => {
+    setTimeout(() => {
+      router.replace('/');
+    }, 4000);
+  }, []);
+
   return (
     <div className='not-found'>
       <h2>💩 404 Error: Page not found</h2>
