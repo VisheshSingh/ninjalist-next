edd418fcdae65c427f2672b63d01d2f901dedce8
Vishesh Singh <VisheshSingh16@gmail.com>
2021-02-03 18:03:43 -0600

    getStaticPaths 👨‍💻

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,5 +1,19 @@
 import React from 'react';
 
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users`);
+  const data = await res.json();
+
+  const paths = data.map((ninja) => ({
+    params: { id: ninja.id.toString() },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
 const DetailsPage = () => {
   return (
     <div>
