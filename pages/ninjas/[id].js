c7bcd0ac4e924f92159d1b39f6c5a062a5b9952a
Vishesh Singh <VisheshSingh16@gmail.com>
2021-02-07 20:04:54 -0600

    geet details page

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -14,10 +14,24 @@ export const getStaticPaths = async () => {
   };
 };
 
-const DetailsPage = () => {
+export const getStaticProps = async (ctx) => {
+  const id = ctx.params.id;
+  const res = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const data = await res.json();
+
+  return {
+    props: { ninja: data },
+  };
+};
+
+const DetailsPage = ({ ninja }) => {
   return (
     <div>
       <h1>Details Page</h1>
+      <p>{ninja.name}</p>
+      <p>{ninja.email}</p>
+      <p>{ninja.website}</p>
+      <p>{ninja.address.city}</p>
     </div>
   );
 };
